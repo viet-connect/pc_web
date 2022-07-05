6c6a9a0e2a8e68793a4f6fbe6439d1cdf6ef8c75
hyunwook <shw@itemscout.io>
2022-07-06 01:37:24 +0900

    feat: firebase 초기설정 - 버전 9.6.1

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import app from '../lib/firebase/initFirebase'
 import styles from '../styles/Home.module.css'
-
+console.log(app);
 export default function Home(home) {
-  console.log("1");
-  console.log9('1');
   return (
     <div>
       <h1>Homepage</h1>
